1217e0013c84e991fe8296e423deda31040b2ecc
Xenia_Popova <p961083@gmail.com>
2024-02-03 16:25:14 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/homemedicalkit/MainScreen.kt
@@ -0,0 +1,4 @@
+package com.example.homemedicalkit
+
+class MainScreen {
+}
---
+++ app/src/main/java/com/example/homemedicalkit/ui/theme/MedicinePage.kt
@@ -0,0 +1,8 @@
+package com.example.homemedicalkit.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class MedicinePage {
+
+}
